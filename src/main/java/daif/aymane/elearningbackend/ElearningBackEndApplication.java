4b034c5647018eb48e7448b7a9bee6e34ac61648
aymane-daif <aymaned345@gmail.com>
2022-06-28 20:17:22 +0100

    :sparkles: setup project

---
+++ src/main/java/daif/aymane/elearningbackend/ElearningBackEndApplication.java
@@ -0,0 +1,13 @@
+package daif.aymane.elearningbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearningBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daif/aymane/elearningbackend/ElearningBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package daif.aymane.elearningbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
